c195d4596f2e138fe6099b065952f07bd30beaeb
Noelle <73260931+No767@users.noreply.github.com>
2023-05-27 00:36:53 -0700

    Fixes

--- chitose/api/routes/ipc.py
+++ chitose/api/routes/ipc.py
@@ -1,14 +1,11 @@
 from typing import Any
 
 from discord.ext.ipc import Client
-from fastapi import APIRouter, BackgroundTasks
+from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
 
-from ..models import CreateTag, CreateTagResponseModel
-from ..tasks import createTag
-
 router = APIRouter(default_response_class=ORJSONResponse)
-ipc = Client(secret_key="")
+ipc = Client(secret_key="test")
 
 
 @router.get("/user/{user_id}")
@@ -18,17 +15,29 @@ async def get_user(user_id: int) -> Any:
     return ipcRes.response
 
 
-@router.post("/tags/create", status_code=202)
-async def create_tag(
-    request: CreateTag, background_task: BackgroundTasks
-) -> CreateTagResponseModel:
-    background_task.add_task(createTag, path="create_tag", data=request)
-    return CreateTagResponseModel(
-        status=202, message="Successfully created tag", data=request
-    )
+# @router.post("/tags/create", status_code=202)
+# async def create_tag(
+#     request: CreateTag, background_task: BackgroundTasks
+# ) -> CreateTagResponseModel:
+#     background_task.add_task(createTag, path="create_tag", data=request)
+#     return CreateTagResponseModel(
+#         status=202, message="Successfully created tag", data=request
+#     )
+
+
+@router.get("/tags/{guild_id}/{name}/content")
+async def get_tag_content(guild_id: int, name: str):
+    ipcRes = await ipc.request("get_tag_content", guild_id=guild_id, tag_name=name)
+    return ipcRes.response
 
 
 @router.get("/tags/search")
 async def get_tag(q: str, guild_id: int) -> Any:
-    ipcRes = await ipc.request("get_tag", name=q, guild_id=guild_id)
+    ipcRes = await ipc.request("get_tag", guild_id=guild_id, tag_name=q)
+    return ipcRes.response
+
+
+@router.get("/tags/{guild_id}/all")
+async def get_tag(guild_id: int):
+    ipcRes = await ipc.request("get_all_guild_tag", guild_id=guild_id)
     return ipcRes.response
--- chitose/main.py
+++ chitose/main.py
@@ -1,21 +1,9 @@
 from api import api_router
 from config.settings import settings
 from fastapi import FastAPI
-from prisma import Prisma
 
 app = FastAPI(
     title=settings.NAME, description=settings.DESCRIPTION, version=settings.VERSION
 )
-db = Prisma(auto_register=True)
 
 app.include_router(api_router)
-
-
-@app.on_event("startup")
-async def startup():
-    await db.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await db.disconnect()
